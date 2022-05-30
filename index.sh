7b04346aec57ba7c10cc7c108122d73ba3759bdd
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-05-31 01:00:55 +0700

    Add files via upload

--- index.sh
+++ index.sh
@@ -7,6 +7,7 @@ cat << HiH | sed2
 <page config-sh=". $PHOME/home.sh" id="$RANDOM" visible="test -e /sdcard/Tools/Vip && echo 1 || echo 0">
 <title>Hack Proxy</title>
 <desc>Giúp máy bạn có mạng, thay đổi ip proxy</desc>
+<option type="refresh" icon="$TOME/usr/Tool/Loading.png" style="fab">$lammoi</option>
 <option type="default" auto-off="true">Cập nhật Config.yaml</option>
 <handler>
 # Start code
