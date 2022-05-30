87abef65e151b350f41965f08194265d717f6dcf
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-05-30 19:42:25 +0700

    Add files via upload

--- scripts/service.sh
+++ scripts/service.sh
@@ -1,1 +1,11 @@
-
+cd $(dirname $0)
+if [ ! -f "/data/data/com.kakathic.shells/files/TOME/Module/Hack-Proxy/run/clash.pid" ];then
+sh clash.service -s
+cat /data/data/com.kakathic.shells/files/TOME/Module/Hack-Proxy/run/run.log
+else
+sh clash.service -k
+cat /data/data/com.kakathic.shells/files/TOME/Module/Hack-Proxy/run/run.log
+fi
+if [ -f "/data/data/com.kakathic.shells/files/TOME/Module/Hack-Proxy/run/error" ];then
+cat /data/data/com.kakathic.shells/files/TOME/Module/Hack-Proxy/run/error
+fi
