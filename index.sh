ecdcbd4bcbd011908408ab02d06273684f1ac651
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-05-30 21:27:30 +0700

    Update index.sh

--- index.sh
+++ index.sh
@@ -4,7 +4,7 @@ PHOME="$TOME/Module/Hack-Proxy"
 # Command
 cat << HiH | sed2
 <group>
-<page config-sh=". $PHOME/home.sh" id="$RANDOM">
+<page config-sh=". $PHOME/home.sh" id="$RANDOM" visible="test -e /sdcard/Tools/Vip && echo 1 || echo 0">
 <title>Hack Proxy</title>
 <desc>Giúp máy bạn có mạng, thay đổi ip proxy</desc>
 <option type="default" auto-off="true">Cập nhật Config.yaml</option>
