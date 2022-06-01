e2a524561aab7d14a1e093b56fbd9ef22476fa20
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-06-01 15:20:15 +0700

    Add files via upload

--- index.sh
+++ index.sh
@@ -9,13 +9,22 @@ cat << HiH | sed2
 <desc>Giúp máy bạn có mạng, thay đổi ip proxy</desc>
 <option type="refresh" icon="$TOME/usr/Tool/Loading.png" >$lammoi</option>
 <option type="default" auto-off="true">Cập nhật Config.yaml</option>
+<option type="default" id="kk2" auto-off="true">Cập nhật toàn bộ</option>
 <option type="default" id="kk" >Nhật ký</option>
 <handler>
 # Start code
 if [ "€menu_id" == "kk" ];then
 cat $PHOME/run/kernel.log
+elif [ "€menu_id" == "kk2" ];then
+ecgi "€loading"
+Taive "https://github.com/kakathic/Hack/archive/refs/heads/Proxy.zip" "€TEMP_DIR/Test.zip"
+unzip -o "€TEMP_DIR/Test.zip" -d "€TOME/Module"
+chmod -R 777 "€TOME/Module"
+rm -fr "€TEMP_DIR/Test.zip
+rm -fr "€TEMP_DIR"/*
 else
-Taive "https://raw.githubusercontent.com/kakathic/Hack/Proxy/config.yaml" "$PHOME/config.yaml" "-#kLJ --connect-timeout 5" "text"
+ecgi "€loading"
+Taive "https://raw.githubusercontent.com/kakathic/Hack/Proxy/config.yaml" "$PHOME/config.yaml"
 fi
 # End code
 </handler></page></group>
