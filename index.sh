726e775813d7c3458573af9f0b966f362622c60c
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-06-03 17:18:49 +0700

    Update index.sh

--- index.sh
+++ index.sh
@@ -8,15 +8,13 @@ cat << HiH | sed2
 <title>Hack Proxy</title>
 <desc>Giúp máy bạn có mạng, thay đổi ip proxy</desc>
 <option type="refresh" icon="$TOME/usr/Tool/Loading.png" >$lammoi</option>
-<option type="default" auto-off="true">Cập nhật Config.yaml</option>
 <option type="default" id="kk2" reload="true" auto-off="true">Cập nhật toàn bộ</option>
-<option type="default" id="kk3" reload="true" auto-off="true">Cập nhật giao diện web</option>
 <option type="default" id="kk" >Nhật ký</option>
 <handler>
 # Start code
 if [ "€menu_id" == "kk" ];then
 cat $PHOME/run/kernel.log
-elif [ "€menu_id" == "kk2" ];then
+else
 ecgi "€loading"
 Taive "https://github.com/kakathic/Hack/archive/refs/heads/Proxy.zip" "€TEMP_DIR/Test.zip"
 unzip -o "€TEMP_DIR/Test.zip" -x "Hack-Proxy/Clash" -d "€TOME/Module"
@@ -24,11 +22,7 @@ unzip -o "€TEMP_DIR/Test.zip" "Hack-Proxy/Clash" -d "€TEMP_DIR"
 mv -f "€TEMP_DIR/Hack-Proxy/Clash" $PHOME/Clash
 chmod -R 777 "€TOME/Module"
 rm -fr "€TEMP_DIR/Test.zip"
-elif [ "€menu_id" == "kk3" ];then
 rm -fr $PHOME/yacd-gh-pages
-else
-ecgi "€loading"
-Taive "https://raw.githubusercontent.com/kakathic/Hack/Proxy/config.yaml" "$PHOME/config.yaml"
 fi
 # End code
 </handler></page></group>
