fe383ce0b8642f4f912fd8287dd617d1dd7b9153
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-06-03 22:01:03 +0700

    Add files via upload

--- home.sh
+++ home.sh
@@ -49,13 +49,17 @@ Url () { echo "€Durl" | sed 's|vmess:\/\/||g' | base64 -d 2>/dev/null | sed -e
 
 [ "€(Url add)" == 'v.akamaized.net' ] && Tck="€(Url host)" || Tck="€(Url add)"
 Ukr="€(Xem http://ip-api.com/csv/€Tck\?fields=query)"
-echo "# Host: €(Url host), €(Url add), €Ukr
-  - { name: \"€Tensv\", type: vmess, server: €Ukr, uuid: €(Url id), port: 80, alterId: 0, cipher: auto, udp: true, tls: true, skip-cert-verify: true, network: ws, ws-opts: { method: \"GET\" path: \"€(echo €(Url path))\", headers: { Host: v.akamaized.net } } }" >> $PHOME/run/Vip.yaml
+echo "#€Tensv
+# Host: €(Url host), €(Url add), €Ukr
+  - { name: \"€Tensv\", type: vmess, server: €Ukr, uuid: €(Url id), port: 80, alterId: 0, cipher: auto, udp: true, tls: true, skip-cert-verify: true, network: ws, ws-opts: { method: \"GET\" path: \"€(echo €(Url path))\", headers: { Host: v.akamaized.net } } }
+##€Tensv" >> $PHOME/run/Vip.yaml
 elif [ €(echo €Durl | grep -cm1 trojan) == 1 ];then
 Jddg="€(echo €Durl | cut -d @ -f2 | cut -d : -f1)"
 Ukr="€(Xem http://ip-api.com/csv/€Jddg\?fields=query)"
-echo "# €Jddg
-  - { name: \"€Tensv\", type: trojan, server: €Ukr, port: 443, password: €(echo €Durl | cut -d / -f3 | cut -d @ -f1), udp: true, sni: v.akamaized.net }" >> $PHOME/run/Vip.yaml
+echo "#€Tensv
+# €Jddg
+  - { name: \"€Tensv\", type: trojan, server: €Ukr, port: 443, password: €(echo €Durl | cut -d / -f3 | cut -d @ -f1), udp: true, sni: v.akamaized.net }
+##€Tensv" >> $PHOME/run/Vip.yaml
 elif [ €Durl ];then
 echo "
 #€Tensv
