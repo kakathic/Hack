cdc8827d65523becd8191f0e5e7c5ec9a7b06bb9
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-06-04 11:32:14 +0700

    Update home.sh

--- home.sh
+++ home.sh
@@ -16,7 +16,6 @@ fi
 if [ ! -e "$CLASH" ];then
 Linkhhf="$(Xem "https://github.com/MetaCubeX/Clash.Meta/releases/tag/Prerelease-Alpha" | grep -m1 'Clash.Meta-android-arm64-alpha' | cut -d \" -f2)"
 Taive "https://github.com$Linkhhf" "$TEMP_DIR/Clash.gz"
-unzip -o "$TEMP_DIR/Test.zip" -d "$TOME/Module"
 gzip -d "$TEMP_DIR/Clash.gz"
 mv -f "$TEMP_DIR/Clash" $PHOME/Clash
 chmod -R 777 "$PHOME"
