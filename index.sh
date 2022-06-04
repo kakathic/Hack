688ed1556b7e5535dbc929831670745bdf8713fd
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-06-04 17:44:39 +0700

    Update index.sh

--- index.sh
+++ index.sh
@@ -7,13 +7,19 @@ cat << HiH | sed2
 <page config-sh=". $PHOME/home.sh" id="$RANDOM" visible="test -e /sdcard/Tools/Vip && echo 1 || echo 0">
 <title>Hack Proxy</title>
 <desc>Giúp máy bạn có mạng, thay đổi ip proxy</desc>
-<option type="refresh" icon="$TOME/usr/Tool/Loading.png" >$lammoi</option>
+<option reload="true" auto-off="true" id="kk3" >$lammoi</option>
 <option type="default" id="kk2" reload="true" auto-off="true">Cập nhật toàn bộ</option>
 <option type="default" id="kk" >Nhật ký</option>
 <handler>
 # Start code
 if [ "€menu_id" == "kk" ];then
 cat $PHOME/run/kernel.log
+elif [ "€menu_id" == "kk3" ];then
+if [ -e $PHOME/run/clash.pid ];then
+$PHOME/scripts/service.sh
+echo
+$PHOME/scripts/service.sh
+fi
 else
 ecgi "€loading"
 Taive "https://github.com/kakathic/Hack/archive/refs/heads/Proxy.zip" "€TEMP_DIR/Test.zip"
