c18a22760dbe7f0be95a37063dd93abe008ed48d
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-06-06 12:22:49 +0700

    Update home.sh

--- home.sh
+++ home.sh
@@ -8,7 +8,7 @@ cp -rf $PHOME/Hack-Proxy.sh /data/adb/service.d 2>/dev/null
 fi
 
 Thongtinkkdh="$(timeout 1 Xem https://raw.githubusercontent.com/kakathic/Hack/Proxy/README.md)"
-[ "$Thongtinkkdh" ] && onsbhd="$Thongtinkkdh" || onsbhd="Vui lòng bật kết nối internet!\nHoặc bật Wi-fi!"
+[ "$Thongtinkkdh" ] && onsbhd="$Thongtinkkdh" || onsbhd="Vui lòng bật kết nối internet!£Hoặc bật Wi-fi!"
 
 if [ ! -e $PHOME/run/Vip.yaml ];then
 mkdir -p "$PHOME/run"
