566505d8628cf426e63f4f94c62dc370ba93dd61
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-06-06 22:00:59 +0700

    Update service.sh

--- scripts/service.sh
+++ scripts/service.sh
@@ -2,7 +2,6 @@ cd $(dirname $0)
 if [ ! -f "$TOME/Module/Hack-Proxy/run/clash.pid" ];then
 sh clash.service -s
 cat $TOME/Module/Hack-Proxy/run/run.log
-curl -s --connect-timeout 2 -X PUT -H "Content-Type: application/json" -d '{"path": "'$PHOME'/config.yaml"}' http://127.0.0.1:9090/configs >/dev/null
 else
 sh clash.service -k
 cat $TOME/Module/Hack-Proxy/run/run.log
