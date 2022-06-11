58d77508423a247911bf8f64a8eb8e2f0ac75469
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-06-11 14:40:47 +0700

    Update home.sh

--- home.sh
+++ home.sh
@@ -113,15 +113,10 @@ fi
 <param name="ksjdbdjdj" desc=" " label="Hệ thống" value-sh="grep -m1 stack: $PHOME/config.yaml | cut -d : -f2" option-sh="echo system; echo gvisor;"/>
 <param name="eheheb" desc=" " label="Nhật ký" value-sh="grep -m1 log-level: $PHOME/config.yaml | cut -d : -f2" option-sh="echo silent; echo info; echo warning; echo error; echo debug;"/>
 <param name="sjdjdh" value-sh="grep -m1 filter: $PHOME/config.yaml | cut -d \&quot; -f2" type="text" desc="£Điền tên cần lọc ở sever Auto, Magic, bỏ trống để hủy lọc££Mặc định: 🇻🇳|VN£" label="Lọc tên"/>
-<param name="bsbeh4j" desc=" " label="Chặn QC" value-sh="test $(grep -cm1 '#  - RULE-SET,Anti-ad,REJECT' $PHOME/config.yaml) == 1 && echo 0 || echo 1" type="switch"/>
-
 <set>
 [ "€Luachokhb" ] && sed -i -e "s/€(grep -m1 mode: $PHOME/config.yaml)/mode: €Luachokhb/g" "$PHOME/config.yaml"
 [ "€ksjdbdjdj" ] && sed -i -e "s/€(grep -m1 stack: $PHOME/config.yaml)/  stack: €ksjdbdjdj/g" "$PHOME/config.yaml"
 [ "€eheheb" ] && sed -i -e "s/€(grep -m1 log-level: $PHOME/config.yaml)/log-level: €eheheb/g" "$PHOME/config.yaml"
-
-[ "€bsbeh4j" == 1 ] && sed -i -e "s/€(grep -m1 'RULE-SET,Anti-ad,REJECT' $PHOME/config.yaml)/  - RULE-SET,Anti-ad,REJECT/g" "$PHOME/config.yaml" || sed -i -e "s/€(grep -m1 'RULE-SET,Anti-ad,REJECT' $PHOME/config.yaml)/#  - RULE-SET,Anti-ad,REJECT/g" "$PHOME/config.yaml"
-
 [ "€sjdjdh" ] && sed -i -e "s/€(grep -m1 filter: $PHOME/config.yaml)/      filter: \"€sjdjdh\"/g" "$PHOME/config.yaml" || sed -i -e "s/€(grep -m1 filter: $PHOME/config.yaml)/#      filter: \"€sjdjdh\"/g" "$PHOME/config.yaml"
 
 curl -s -X PUT -H "Content-Type: application/json" -d '{"path": "$PHOME/config.yaml"}' http://127.0.0.1:9090/configs
